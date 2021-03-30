8bc0ecb9e4c5f1a73f2d0702e18c1932e11ffecf
Neha code <nehakumarikatras@gmail.com>
2021-03-30 22:26:05 +0530

    deploy SpaceXApp

---
+++ server.js
@@ -0,0 +1,12 @@
+// server.js
+const express = require('express');
+const path =require('express')
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist/SpaceXApp'));
+// Start the app by listening on the default
+// Heroku port
+app.get('/*',(req,res)); 
+res.sendFile('index.html',{root:'dist/SpaceXApp/'})
+app.listen(process.env.PORT || 8080);
